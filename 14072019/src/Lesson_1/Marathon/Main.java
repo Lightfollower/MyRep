9617ebe9f6c412425f0a6527dcd59cdd518d4174
Doom <lightfollower1985@gmail.com>
2019-07-15 20:13:52 +0500

    Сделал ДЗ, пошёл за пивом.

--- 14072019/src/Lesson_1/Marathon/Main.java
+++ 14072019/src/Lesson_1/Marathon/Main.java
@@ -1,17 +1,18 @@
 package Lesson_1.Marathon;
 
+import Lesson_1.Marathon.competitors.Team;
+import Lesson_1.Marathon.obstacles.Course;
+import Lesson_1.Marathon.obstacles.Cross;
+import Lesson_1.Marathon.obstacles.Wall;
+import Lesson_1.Marathon.competitors.Cat;
+import Lesson_1.Marathon.competitors.Dog;
+import Lesson_1.Marathon.competitors.Human;
+
 public class Main {
     public static void main(String[] args) {
-        Competitor[] competitors = {new Human("Боб"), new Cat("Барсик"), new Dog("Бобик")};
-        Obstacle[] course = {new Cross(80), new Wall(2), new Wall(1), new Cross(120)};
-        for (Competitor c : competitors) {
-            for (Obstacle o : course) {
-                o.doIt(c);
-                if (!c.isOnDistance()) break;
-            }
-        }
-        for (Competitor c : competitors) {
-            c.info();
-        }
+        Team team = new Team("Human, dog and cat", new Human("Боб"), new Cat("Барсик"), new Dog("Бобик"));
+        Course course = new Course(new Cross(80), new Wall(2), new Wall(1), new Cross(120));
+        course.teamDoIt(team);
+        team.activeCompetitorsInfo();
     }
 }
