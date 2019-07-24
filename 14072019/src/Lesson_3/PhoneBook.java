b2392de7f3f020d593cc668a155baa591cd02e10
Doom <lightfollower1985@gmail.com>
2019-07-24 16:27:18 +0500

    Заменил ArrayList на HashSet

--- 14072019/src/Lesson_3/PhoneBook.java
+++ 14072019/src/Lesson_3/PhoneBook.java
@@ -1,20 +1,20 @@
 package Lesson_3;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 public class PhoneBook {
-    Map<String, ArrayList<Integer>> phoneBook = new HashMap<>();
+    Map<String, HashSet<Integer>> phoneBook = new HashMap<>();
 
     public void add(String name, int number) {
         if (!phoneBook.containsKey(name))
-            phoneBook.put(name, new ArrayList<Integer>());
+            phoneBook.put(name, new HashSet<Integer>());
 
         phoneBook.get(name).add(number);
     }
 
-    public ArrayList<Integer> get(String name) {
+    public HashSet<Integer> get(String name) {
         System.out.println(phoneBook.get(name));
         return phoneBook.get(name);
     }
