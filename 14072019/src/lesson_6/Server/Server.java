94d5bcd93159cb7723d429013920a430f055d318
Doom <lightfollower1985@gmail.com>
2019-08-02 14:04:07 +0500

    ДЗ на 31.07

---
+++ 14072019/src/lesson_6/Server/Server.java
@@ -0,0 +1,59 @@
+package lesson_6.Server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Server {
+    private Vector<ClientHandler> clients;
+
+    public Server() {
+        clients = new Vector<>();
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен!");
+
+            while (true) {
+                socket = server.accept();
+                broadcastMsg("User " + socket.toString() + " added");
+                clients.add(new ClientHandler(this, socket));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void broadcastMsg(String msg) {
+        for (ClientHandler o: clients) {
+            o.sendMsg(msg);
+        }
+    }
+
+    void modifyClients (ClientHandler client){
+        clients.remove(client);
+    }
+    void showClients(){
+        System.out.println(clients);
+    }
+}
