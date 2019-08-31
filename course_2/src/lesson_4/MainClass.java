b2769dabef9dc5f0c220a8aa880f7220123ec69a
Doom <lightfollower1985@gmail.com>
2019-08-31 23:33:04 +0500

    Нити печатают букафки, по очереди.

---
+++ course_2/src/lesson_4/MainClass.java
@@ -0,0 +1,37 @@
+package lesson_4;
+
+public class MainClass {
+    static int turn = 0;
+
+    public static void main(String[] args) {
+        createAndRunThreads(new char[]{'a', 'b', 'c', 'd', 'e' , 'f'});
+    }
+
+    public static void createAndRunThreads(char... chars) {
+        for (int i = 0; i < chars.length; i++) {
+            int iteration = i;
+            new Thread(new Runnable() {
+                int number = iteration;
+
+                char letter = chars[iteration];
+                @Override
+                public void run() {
+                    synchronized (System.out) {
+                        for (int j = 0; j < 5; j++) {
+                        while (turn != iteration) {
+                            try {
+                                System.out.wait();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                            System.out.println(letter);
+                            turn  = (number == chars.length - 1) ? 0 : number + 1;
+                            System.out.notifyAll();
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
+}
