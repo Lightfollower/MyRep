5c6d87f6244f123411e35f8afca0cde332cb02cf
Doom <lightfollower1985@gmail.com>
2019-09-04 14:45:50 +0500

    ДЗ на 02.09

---
+++ course_2/src/lesson_5/MainClass.java
@@ -0,0 +1,40 @@
+package lesson_5;
+
+import java.util.concurrent.CountDownLatch;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    private static CountDownLatch start = new CountDownLatch(CARS_COUNT);
+
+    protected static CountDownLatch finita = new CountDownLatch(CARS_COUNT);
+
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(CARS_COUNT, new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(start, race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        try {
+            start.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        try {
+            finita.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
+
+
+
